a1e61820bd15fa56059c167f5a9517df1da191e1
Matheus <mrogeriopesarini@gmail.com>
2025-02-26 22:57:57 -0300

    adiciona middleware de sessão, configurações do Prisma e rotas para gerenciamento de usuários e produtos

--- api/routes/login-user/login.ts
+++ api/routes/login-user/login.ts
@@ -4,7 +4,7 @@ import type { FastifyInstance } from "fastify";
 import { PrismaClient } from "@prisma/client";
 import { z } from "zod";
 import argon2 from "argon2";
-import { createUserSession } from "@/lib/cookie/session";
+import { createUserSession } from "../../middleware/session";
 
 const prisma = new PrismaClient();
 
